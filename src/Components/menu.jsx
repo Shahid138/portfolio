c72186896dcb10581e5c8303943ba800bc50de9b
Shahid <shahidsaifi138@example.com>
2024-06-06 14:45:27 +0530

    Resume update

--- src/Components/menu.jsx
+++ src/Components/menu.jsx
@@ -42,7 +42,7 @@ const Menu = ({ menuToggle }) => {
         </li>
         <li className="menu-item">
           <a
-            href="/Portfolio/docs/Mayank_Resume.pdf"
+            href="/Portfolio-main/docs/Shahid_Resume.pdf"
             target="_blank"
             rel="noreferrer"
             draggable="false"
